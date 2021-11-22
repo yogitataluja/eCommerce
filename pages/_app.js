9a9fb84c615962dcfe8aa1977baa87cd86cdf867
yogita-taluja <yogita.taluja21@gmail.com>
2021-11-22 21:41:37 +0530

    fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Script from 'next/script'
 // import "../public/assets/css/base.css";
 // import "../public/assets/css/defined.css";
 // import "../public/assets/css/bootstrap.min.css";
@@ -15,6 +16,43 @@ import Layout from '../components/Layout'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+
+{/* <Script  src="/assets/js/interface.js"></Script> */}
+          {/* <Script
+            type="text/javascript"
+            src="/assets/js/jquery.hoverIntent.min.js"
+          ></Script> */}
+          <Script src="/assets/js/jquery-3.5.1.min.js"></Script>
+          <Script src="/assets/js/nouislider.min.js"></Script>
+          <Script src="/assets/js/jarallax.min.js"></Script>
+          <Script src="/assets/js/packery.pkgd.min.js"></Script>
+          <Script src="/assets/js/magnific-popup.min.js"></Script>
+          <Script src="/assets/js/flickity.pkgd.min.js"></Script>
+          <Script src="/assets/js/lazysizes.min.js"></Script>
+          <Script src="/assets/js/js-cookie.min.js"></Script>
+          <Script src="/assets/js/jquery.countdown.min.js"></Script>
+          <Script src="/assets/js/photoswipe.min.js"></Script>
+          <Script src="/assets/js/photoswipe-ui-default.min.js"></Script>
+          <Script src="/assets/js/drift.min.js"></Script>
+          <Script src="/assets/js/isotope.pkgd.min.js"></Script>
+          <Script src="/assets/js/resize-sensor.min.js"></Script>
+          <Script src="/assets/js/theia-sticky-sidebar.min.js"></Script>
+
+          <Script
+            src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossOrigin="anonymous"
+          ></Script>
+          <Script
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossOrigin="anonymous"
+          ></Script>
+          <Script
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossOrigin="anonymous"
+          ></Script>
     <Layout> 
       <Component {...pageProps} />
       </Layout>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -37,42 +37,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script type="text/javascript" src="/assets/js/interface.js"></Script>
-          <Script
-            type="text/javascript"
-            src="/assets/js/jquery.hoverIntent.min.js"
-          ></Script>
-          <Script src="/assets/js/jquery-3.5.1.min.js"></Script>
-          <Script src="/assets/js/nouislider.min.js"></Script>
-          <Script src="/assets/js/jarallax.min.js"></Script>
-          <Script src="/assets/js/packery.pkgd.min.js"></Script>
-          <Script src="/assets/js/magnific-popup.min.js"></Script>
-          <Script src="/assets/js/flickity.pkgd.min.js"></Script>
-          <Script src="/assets/js/lazysizes.min.js"></Script>
-          <Script src="/assets/js/js-cookie.min.js"></Script>
-          <Script src="/assets/js/jquery.countdown.min.js"></Script>
-          <Script src="/assets/js/photoswipe.min.js"></Script>
-          <Script src="/assets/js/photoswipe-ui-default.min.js"></Script>
-          <Script src="/assets/js/drift.min.js"></Script>
-          <Script src="/assets/js/isotope.pkgd.min.js"></Script>
-          <Script src="/assets/js/resize-sensor.min.js"></Script>
-          <Script src="/assets/js/theia-sticky-sidebar.min.js"></Script>
-
-          <Script
-            src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossOrigin="anonymous"
-          ></Script>
-          <Script
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-            crossOrigin="anonymous"
-          ></Script>
-          <Script
-            src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-            crossOrigin="anonymous"
-          ></Script>
+          
         </body>
       </Html>
     );
