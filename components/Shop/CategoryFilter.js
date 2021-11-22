005af7378bca817d9af978688738912170dc060c
yogita-taluja <yogita.taluja21@gmail.com>
2021-11-22 21:32:24 +0530

    fix de bug

--- components/Shop/CategoryFilter.js
+++ components/Shop/CategoryFilter.js
@@ -25,26 +25,26 @@ const router = useRouter()
     return (
         <>
           {/* <!--left sidebar--> */}
-          <div class="js_sidebar sidebar sidebar_nt col-lg-3 col-12 space_30 hidden_false lazyload">
+          <div className="js_sidebar sidebar sidebar_nt col-lg-3 col-12 space_30 hidden_false lazyload">
       <div
         id="kalles-section-sidebar_shop"
-        class="kalles-section nt_ajaxFilter section_sidebar_shop type_instagram"
+        className="kalles-section nt_ajaxFilter section_sidebar_shop type_instagram"
       >
-        <div class="h3 mg__0 tu bgb cw visible-sm fs__16 pr">
-          Sidebar<i class="close_pp pegk pe-7s-close fs__40 ml__5"></i>
+        <div className="h3 mg__0 tu bgb cw visible-sm fs__16 pr">
+          Sidebar<i className="close_pp pegk pe-7s-close fs__40 ml__5"></i>
         </div>
-        <div class="cat_shop_wrap">
-          <div class="cat_fixcl-scroll">
-            <div class="cat_fixcl-scroll-content css_ntbar">
-              <div class="row no-gutters wrap_filter">
-                <div class="col-12 col-md-12 widget widget_product_categories cat_count_false">
-                  <div class="col-12 col-md-12 widget">
-                    <h5 class="widget-title">Filter by price</h5>
-                    <div class="loke_scroll">
-                      <ul class="nt_filter_block nt_filter_styleck css_ntbar">
+        <div className="cat_shop_wrap">
+          <div className="cat_fixcl-scroll">
+            <div className="cat_fixcl-scroll-content css_ntbar">
+              <div className="row no-gutters wrap_filter">
+                <div className="col-12 col-md-12 widget widget_product_categories cat_count_false">
+                  <div className="col-12 col-md-12 widget">
+                    <h5 className="widget-title">Filter by price</h5>
+                    <div className="loke_scroll">
+                      <ul className="nt_filter_block nt_filter_styleck css_ntbar">
                          
-                      {Pdata.map((item)=>
-                        <li style={{display:"flex", alignItems:"center", textAlign:"left"}}>
+                      {Pdata.map((item, index)=>
+                        <li key={index} style={{display:"flex", alignItems:"center", textAlign:"left"}}>
                           <input type="radio" id={item._id} name="50-100" value={item._id}
                             aria-label="Narrow selection to products matching tag price Rs. 50-Rs. 100" onChange={e => handleCheckBox(item.min, item.max)} style={{border:"none", textDecoration:"none"}}
                           />
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import Script from 'next/script'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -37,42 +37,42 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script type="text/javascript" src="/assets/js/interface.js"></script>
-          <script
+          <Script type="text/javascript" src="/assets/js/interface.js"></Script>
+          <Script
             type="text/javascript"
             src="/assets/js/jquery.hoverIntent.min.js"
-          ></script>
-          <script src="/assets/js/jquery-3.5.1.min.js"></script>
-          <script src="/assets/js/nouislider.min.js"></script>
-          <script src="/assets/js/jarallax.min.js"></script>
-          <script src="/assets/js/packery.pkgd.min.js"></script>
-          <script src="/assets/js/magnific-popup.min.js"></script>
-          <script src="/assets/js/flickity.pkgd.min.js"></script>
-          <script src="/assets/js/lazysizes.min.js"></script>
-          <script src="/assets/js/js-cookie.min.js"></script>
-          <script src="/assets/js/jquery.countdown.min.js"></script>
-          <script src="/assets/js/photoswipe.min.js"></script>
-          <script src="/assets/js/photoswipe-ui-default.min.js"></script>
-          <script src="/assets/js/drift.min.js"></script>
-          <script src="/assets/js/isotope.pkgd.min.js"></script>
-          <script src="/assets/js/resize-sensor.min.js"></script>
-          <script src="/assets/js/theia-sticky-sidebar.min.js"></script>
+          ></Script>
+          <Script src="/assets/js/jquery-3.5.1.min.js"></Script>
+          <Script src="/assets/js/nouislider.min.js"></Script>
+          <Script src="/assets/js/jarallax.min.js"></Script>
+          <Script src="/assets/js/packery.pkgd.min.js"></Script>
+          <Script src="/assets/js/magnific-popup.min.js"></Script>
+          <Script src="/assets/js/flickity.pkgd.min.js"></Script>
+          <Script src="/assets/js/lazysizes.min.js"></Script>
+          <Script src="/assets/js/js-cookie.min.js"></Script>
+          <Script src="/assets/js/jquery.countdown.min.js"></Script>
+          <Script src="/assets/js/photoswipe.min.js"></Script>
+          <Script src="/assets/js/photoswipe-ui-default.min.js"></Script>
+          <Script src="/assets/js/drift.min.js"></Script>
+          <Script src="/assets/js/isotope.pkgd.min.js"></Script>
+          <Script src="/assets/js/resize-sensor.min.js"></Script>
+          <Script src="/assets/js/theia-sticky-sidebar.min.js"></Script>
 
-          <script
+          <Script
             src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
             integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
             crossOrigin="anonymous"
-          ></script>
-          <script
+          ></Script>
+          <Script
             src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
             integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
             crossOrigin="anonymous"
-          ></script>
-          <script
+          ></Script>
+          <Script
             src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
             crossOrigin="anonymous"
-          ></script>
+          ></Script>
         </body>
       </Html>
     );
