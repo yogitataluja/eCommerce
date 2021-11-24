aa7eab4c4b987d370310387bf8ada69d12f173a1
yogita-taluja <yogita.taluja21@gmail.com>
2021-11-24 16:24:01 +0530

    fix bug

--- pages/productdetails/[productId].js
+++ pages/productdetails/[productId].js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import ReactImageMagnify from "react-image-magnify";
 import {API} from "../../components/API"
 import Navigation from "../../components/Navigation"
+import Link from 'next/link'
 
 const ProductId = ({productId}) => {
     const myref= useRef(null)
@@ -28,7 +29,7 @@ const ProductId = ({productId}) => {
         setImage(product.data.product.image);
       }
       getProduct();
-    }, []);
+    }, [productId]);
     console.log(productDetailes, "PRODUCT DETAILS");
   
     // function addCart(id, quantity) {
@@ -66,7 +67,9 @@ const ProductId = ({productId}) => {
               <div className="row al_center">
                 <div className="col">
                   <nav className="sp-breadcrumb">
-                    <a href="/">Home</a>
+                  <Link href="/">
+                    <a >Home</a>
+                    </Link>
                     <i className="facl facl-angle-right"></i>
                     <a href={`/allProducts/${productDetailes?.category}`}>
                       {productDetailes?.category}
@@ -279,8 +282,8 @@ const ProductId = ({productId}) => {
                     <span className="cb">Tags:</span>{" "}
                     {productDetailes.tags != undefined && (
                       <>
-                        {productDetailes.tags.map((i) => (
-                          <a href="shop-filter-options.html" className="cg">
+                        {productDetailes.tags.map((i, index) => (
+                          <a href="shop-filter-options.html" className="cg" key={index}>
                             {i}
                           </a>
                         ))}
@@ -303,7 +306,7 @@ const ProductId = ({productId}) => {
                         <g>
                           <path
                             d="M22 5.16c-.406-.054-1.806-.16-3.43-.16-3.4 0-5.733 1.825-5.733 5.17v2.882H9v3.913h3.837V27h4.604V16.965h3.823l.587-3.913h-4.41v-2.5c0-1.123.347-1.903 2.198-1.903H22V5.16z"
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                           ></path>
                         </g>
                       </svg>
@@ -320,7 +323,7 @@ const ProductId = ({productId}) => {
                         <g>
                           <path
                             d="M27.996 10.116c-.81.36-1.68.602-2.592.71a4.526 4.526 0 0 0 1.984-2.496 9.037 9.037 0 0 1-2.866 1.095 4.513 4.513 0 0 0-7.69 4.116 12.81 12.81 0 0 1-9.3-4.715 4.49 4.49 0 0 0-.612 2.27 4.51 4.51 0 0 0 2.008 3.755 4.495 4.495 0 0 1-2.044-.564v.057a4.515 4.515 0 0 0 3.62 4.425 4.52 4.52 0 0 1-2.04.077 4.517 4.517 0 0 0 4.217 3.134 9.055 9.055 0 0 1-5.604 1.93A9.18 9.18 0 0 1 6 23.85a12.773 12.773 0 0 0 6.918 2.027c8.3 0 12.84-6.876 12.84-12.84 0-.195-.005-.39-.014-.583a9.172 9.172 0 0 0 2.252-2.336"
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                           ></path>
                         </g>
                       </svg>
@@ -335,7 +338,7 @@ const ProductId = ({productId}) => {
                         className="at-icon at-icon-email kalles-social-media__btn"
                       >
                         <g>
-                          <g fill-rule="evenodd"></g>
+                          <g fillRule="evenodd"></g>
                           <path d="M27 22.757c0 1.24-.988 2.243-2.19 2.243H7.19C5.98 25 5 23.994 5 22.757V13.67c0-.556.39-.773.855-.496l8.78 5.238c.782.467 1.95.467 2.73 0l8.78-5.238c.472-.28.855-.063.855.495v9.087z"></path>
                           <path d="M27 9.243C27 8.006 26.02 7 24.81 7H7.19C5.988 7 5 8.004 5 9.243v.465c0 .554.385 1.232.857 1.514l9.61 5.733c.267.16.8.16 1.067 0l9.61-5.733c.473-.283.856-.96.856-1.514v-.465z"></path>
                         </g>
@@ -353,7 +356,7 @@ const ProductId = ({productId}) => {
                         <g>
                           <path
                             d="M7 13.252c0 1.81.772 4.45 2.895 5.045.074.014.178.04.252.04.49 0 .772-1.27.772-1.63 0-.428-1.174-1.34-1.174-3.123 0-3.705 3.028-6.33 6.947-6.33 3.37 0 5.863 1.782 5.863 5.058 0 2.446-1.054 7.035-4.468 7.035-1.232 0-2.286-.83-2.286-2.018 0-1.742 1.307-3.43 1.307-5.225 0-1.092-.67-1.977-1.916-1.977-1.692 0-2.732 1.77-2.732 3.165 0 .774.104 1.63.476 2.336-.683 2.736-2.08 6.814-2.08 9.633 0 .87.135 1.728.224 2.6l.134.137.207-.07c2.494-3.178 2.405-3.8 3.533-7.96.61 1.077 2.182 1.658 3.43 1.658 5.254 0 7.614-4.77 7.614-9.067C26 7.987 21.755 5 17.094 5 12.017 5 7 8.15 7 13.252z"
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                           ></path>
                         </g>
                       </svg>
@@ -370,7 +373,7 @@ const ProductId = ({productId}) => {
                         <g>
                           <path
                             d="M16 6C9.925 6 5 10.56 5 16.185c0 3.205 1.6 6.065 4.1 7.932V28l3.745-2.056c1 .277 2.058.426 3.155.426 6.075 0 11-4.56 11-10.185C27 10.56 22.075 6 16 6zm1.093 13.716l-2.8-2.988-5.467 2.988 6.013-6.383 2.868 2.988 5.398-2.987-6.013 6.383z"
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                           ></path>
                         </g>
                       </svg>
